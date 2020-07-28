9e930de061f135163f9679eeb9d9c6f07d282d64
rancid <robmgraham13@yahoo.com>
2020-07-27 18:44:43 -0700

    tallying correct answers works with score

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
 
 //          COUNTDOWN TIMER
-var startTimer =document.querySelector(".startButton");
+var startTimer = document.querySelector(".startButton");
+var timeleft = 0;
+startTimer.addEventListener("click", function () {
 
-startTimer.addEventListener("click", function(timeleft){
-
-var timeleft = 60;
-var downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "Finished";alert("your score is" + score);// maybe add an alert with score
-  } else {
-    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-  }
-  timeleft -= 1; 
-}, 1000)});
-
-
-var score = 0
-function displayScore() {
-  document.getElementById("score").textContent=score;
-  }
-  displayScore()
+   timeleft = 60;
+  var downloadTimer = setInterval(function () {
+    if (timeleft <= 0) {
+      clearInterval(downloadTimer);
+      document.getElementById("countdown").innerHTML = "Finished"; alert("your score is" + score);// maybe add an alert with score
+    } else {
+      document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+    }
+    timeleft -= 1;
+    console.log(timeleft);
+  }, 1000);
   
+});
 
-function answerValidation(event) {
+
+
+var score = 0;
+
+
+function displayScore() {
+  document.getElementById("score").textContent = score;
+  document.querySelector(".container").addEventListener("click", function (event) {
+    event.stopPropagation();
+    var right = event.target.getAttribute("class");
+    if (right === "correct") {
+      score++;
+      displayScore();
+    }
+    if (right === "wrong") {
+      timeleft -= 5 ;console.log(timeleft);
+    }
+  });
  
-  console.log(this)
 }
+displayScore();
+ 
+
+
+  // (document.getElementsByClassName("container")).addEventListener("click", function (event) {
+  //   event.stopPropagation();
+  //   var right = event.target.getAttribute("class");
+  //   if (right === "correct") {
+  //     score++;
+  //     displayScore();
+  //   }
+  //   if (right === "wrong") {
+  //     timeleft -= 5;
+  //   }
+  // });
