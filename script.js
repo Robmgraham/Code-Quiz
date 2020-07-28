b1b12e38696d28577bd26f31a8bd6b35161b12c0
rancid <robmgraham13@yahoo.com>
2020-07-28 00:08:22 -0700

    added functions to start buttons and to next buttons. also added finish button

--- script.js
+++ script.js
@@ -3,22 +3,48 @@
 var startTimer = document.querySelector(".startButton");
 var timeleft = 0;
 startTimer.addEventListener("click", function () {
-
-   timeleft = 60;
+  timeleft = 60;
   var downloadTimer = setInterval(function () {
     if (timeleft <= 0) {
       clearInterval(downloadTimer);
-      document.getElementById("countdown").innerHTML = "Finished"; alert("your score is" + score);// maybe add an alert with score
+      document.getElementById("countdown").innerHTML = "Finished";
     } else {
-      document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+      document.getElementById("countdown").innerHTML = timeleft + "seconds remaining";
     }
     timeleft -= 1;
     console.log(timeleft);
   }, 1000);
-  
+
 });
 
 
+
+var Q1 = document.querySelector(".startButton")
+Q1.addEventListener("click", function () {
+  document.body.children[3].childNodes[3].style.display = "block"; //first question appears
+  document.body.children[4].style.display = "block"; //next button appears
+
+
+
+  var Q2 = document.querySelector(".nextButton")
+  Q2.addEventListener("click", function () {
+    document.body.children[3].children[1].style.display = "block";  // second question appears
+    document.body.children[3].childNodes[3].style.display = "none"; // first question dissapears
+    document.body.children[4].style.display = "none"; //next button dissapears
+    document.body.children[5].style.display = "block"; //finish button appears
+
+  });
+
+  //what does the finish button do.
+  var fin = (document.body.children[5]);
+  fin.addEventListener("cick", function () {
+    document.body.children[5].style.display = "none"; //finish button dissapears
+    document.body.children[3].children[1].style.display = "none"; // second question dissapears
+  
+  });
+
+
+});
 
 var score = 0;
 
@@ -33,23 +59,10 @@ function displayScore() {
       displayScore();
     }
     if (right === "wrong") {
-      timeleft -= 5 ;console.log(timeleft);
+      timeleft -= 30; console.log(timeleft);
     }
+
   });
- 
+
 }
 displayScore();
- 
-
-
-  // (document.getElementsByClassName("container")).addEventListener("click", function (event) {
-  //   event.stopPropagation();
-  //   var right = event.target.getAttribute("class");
-  //   if (right === "correct") {
-  //     score++;
-  //     displayScore();
-  //   }
-  //   if (right === "wrong") {
-  //     timeleft -= 5;
-  //   }
-  // });
